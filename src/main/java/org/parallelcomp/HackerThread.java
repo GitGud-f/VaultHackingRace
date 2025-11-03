2902c91871948891b7d57ad93be92d3e3d2357e2
Mayas AlKhateeb <alkhateebmayas997@gmail.com>
2025-11-03 17:07:14 +0300

    Add abstract Hacker Thread
    Add this abstract class so everyhackeer implements his strategy of guessing

--- src/main/java/org/parallelcomp/HackerThread.java
+++ src/main/java/org/parallelcomp/HackerThread.java
@@ -1,4 +1,23 @@
 package org.parallelcomp;
 
-public class HackerThread {
+public abstract class HackerThread extends Thread {
+    protected final Vault vault;
+
+    public HackerThread(Vault vault) {
+        this.vault = vault;
+    }
+
+    // Abstract method: each subclass defines its own guessing algorithm
+    public abstract void guessPassword();
+
+    @Override
+    public final void run() {
+        guessPassword(); // enforce that run() calls the strategy
+    }
+
+    // Helper to report success and terminate cleanly
+    protected void reportSuccess(int guess) {
+        System.out.println(this.getName() + " guessed the password: " + guess);
+        System.exit(0);
+    }
 }
