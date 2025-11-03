9e1115aedd601447f4c728be86a017b55f9d1ea0
Mayas AlKhateeb <alkhateebmayas997@gmail.com>
2025-11-03 17:08:23 +0300

    Add implemenntation of Ascending and Descending ALgorithms

--- src/main/java/org/parallelcomp/AscendingHackerThread.java
+++ src/main/java/org/parallelcomp/AscendingHackerThread.java
@@ -1,4 +1,17 @@
 package org.parallelcomp;
 
-public class AscendingHackerThread {
+public class AscendingHackerThread extends HackerThread {
+    public AscendingHackerThread(Vault vault) {
+        super(vault);
+        this.setName("AscendingHacker");
+    }
+
+    @Override
+    public void guessPassword() {
+        for (int guess = 0; guess <= 9999; guess++) {
+            if (vault.isCorrectPassword(guess)) {
+                reportSuccess(guess);
+            }
+        }
+    }
 }
--- src/main/java/org/parallelcomp/DescendingHackerThread.java
+++ src/main/java/org/parallelcomp/DescendingHackerThread.java
@@ -1,4 +1,17 @@
 package org.parallelcomp;
 
-public class DescendingHackerThread {
+public class DescendingHackerThread extends HackerThread {
+    public DescendingHackerThread(Vault vault) {
+        super(vault);
+        this.setName("DescendingHacker");
+    }
+
+    @Override
+    public void guessPassword() {
+        for (int guess = 9999; guess >= 0; guess--) {
+            if (vault.isCorrectPassword(guess)) {
+                reportSuccess(guess);
+            }
+        }
+    }
 }
