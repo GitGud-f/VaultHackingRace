2e34abe7ac042b1cb600566141ed965f3f50b11a
Mayas AlKhateeb <alkhateebmayas997@gmail.com>
2025-11-03 17:26:48 +0300

    feat: take input from user

--- src/main/java/org/parallelcomp/Main.java
+++ src/main/java/org/parallelcomp/Main.java
@@ -1,22 +1,35 @@
 package org.parallelcomp;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Vault vault = new Vault(4500); // Change as needed
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the vault password (0–9999): ");
+        int password = scanner.nextInt();
+
+        if (password < 0 || password > 9999) {
+            System.err.println("Invalid password! Must be between 0 and 9999.");
+            System.exit(1);
+        }
+
+        Vault vault = new Vault(password); // Change as needed
 
         HackerThread asc = new AscendingHackerThread(vault);
         HackerThread desc = new DescendingHackerThread(vault);
         // HackerThread bins = new BinarySearchHackerThread(vault);
+        // HackerThread random = new RandomHackerThread(vault);
         PoliceThread police = new PoliceThread();
 
-        asc.setPriority(Thread.MAX_PRIORITY);
         desc.setPriority(Thread.MAX_PRIORITY);
+        asc.setPriority(Thread.MAX_PRIORITY);
         // bins.setPriority(Thread.MAX_PRIORITY);
+        // random.setPriority(Thread.MAX_PRIORITY);
 
         police.start();
         asc.start();
         desc.start();
         // bins.start();
+        // random.start();
     }
 }
